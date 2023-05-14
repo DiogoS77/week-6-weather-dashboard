8a9b0694ff635d514039766bb22fdfa70c89c4b5
DiogoS77 <diogo.messi.18@hotmail.com>
2023-05-14 01:08:20 +0100

    funct getLocation,clearError, setlocationerror add

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,28 @@
 const WEATHER_API_BASE_URL = "https://api.openweathermap.org";
 const WEATHER_API_KEY = "f23ee9deb4e1a7450f3157c44ed020e1";
 const MAX_DAILY_FORECAST = 5;
+
+let recentLocations = [];
+
+function getLocation() {
+  const userLocation = locationInput.value.trim();
+
+  if (!userLocation) {
+    setLocationError("Please enter a location");
+    return;
+  }
+
+  lookupLocation(userLocation);
+}
+
+function clearError() {
+  const errorDisplay = document.getElementById("error");
+  errorDisplay.textContent = "";
+}
+
+function setLocationError(text) {
+  const errorDisplay = document.getElementById("error");
+  errorDisplay.textContent = text;
+
+  setTimeout(clearError, 3000);
+}
